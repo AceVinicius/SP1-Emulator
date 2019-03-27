3dde273055758a9e5bdfc0959779beea7c070a4e
Vinícius Ferreira Aguiar <40642605+AceVinicius@users.noreply.github.com>
2019-03-27 11:03:54 -0400

    Update and rename compile_debian.sh to Dependencies_debian.sh

--- Dependencies_debian.sh
+++ Dependencies_debian.sh
@@ -8,16 +8,5 @@ sudo apt install bison
 sudo apt install flex
 
 
-# Compile Assembler
-cd ./assembler/
-make
-make tab
-
-
-# Run assembler to compile your assembly file
-./sp1a ../assemblies/loop.asm -o ./test.bin
-cd ../
-
-
 # Compile and run SP1 Emulator
 bash run.sh
