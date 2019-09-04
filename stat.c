0c9eb34f533a5faa01f7768f9bfe92a1a0525644
Vinícius Ferreira Aguiar <40642605+AceVinicius@users.noreply.github.com>
2019-09-04 16:32:42 -0300

    Add files via upload

---
+++ stat.c
@@ -0,0 +1,68 @@
+# include "./headers/include.h"
+# include "./headers/define.h"
+# include "./headers/stat.h"
+
+
+
+void
+setStat(uint8_t *stat,
+        uint8_t  test,
+        int32_t  var )
+{
+    *stat = 0;
+    
+    *stat += zeroACC(var);
+
+    if(test == OVERFLOW)
+    {
+        *stat += overflow(var);
+    }
+    else if(test == CARRY)
+    {
+        *stat += carry(var);
+    }
+
+
+    DEBUG( printf("DEBUG:  O:%2c  C:%2c  Z:%2c\n", *stat & OVERFLOW ? '1' : '0', *stat & CARRY ? '1' : '0', *stat & ZEROACC ? '1' : '0'); )
+
+    return;
+}
+
+
+
+uint8_t
+zeroACC(int32_t var)
+{   
+    if(var == 0)
+    {
+        return ZEROACC;
+    }
+
+    return OK;
+}
+
+
+
+uint8_t
+carry(int32_t var)
+{   
+    if(var > 255 || var < 0)
+    {
+        return CARRY;
+    }
+
+    return OK;
+}
+
+
+
+uint8_t
+overflow(int32_t var)
+{   
+    if(var == 0)
+    {
+        return OVERFLOW;
+    }
+
+    return OK;
+}
