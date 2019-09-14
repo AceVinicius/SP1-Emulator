15bc09fa932720062924243129b7c91c0b8cf66c
Vinícius Ferreira Aguiar <40642605+AceVinicius@users.noreply.github.com>
2019-09-14 01:09:44 -0300

    Update stat.h

--- lib/include/stat.h
+++ lib/include/stat.h
@@ -1,18 +1,19 @@
 #ifndef STAT_H
 # define STAT_H
+
 # include "./define.h"
 
 
 
-void  setStat   (byte    *stat,
-                 byte     test,
-                 int32_t  var );
+void  setStat   ( byte    *stat ,
+                  byte     test ,
+                  int32_t  var  );
 
-byte  zeroACC   (int32_t var);
+byte  zeroACC   ( int32_t var );
 
-byte  carry     (int32_t var);
+byte  carry     ( int32_t var );
 
-byte  overflow  (int32_t var);
+byte  overflow  ( int32_t var );
 
 
 
