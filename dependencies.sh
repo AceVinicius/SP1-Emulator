c53f141f2b2af2484917d3235d46c1f4657892e9
Vinícius Ferreira Aguiar <40642605+AceVinicius@users.noreply.github.com>
2020-02-26 10:57:41 -0300

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -2,10 +2,7 @@
 
 # Install dependencies
 sudo dnf install make
-sudo dnf install gcc
+sudo dnf install clang
 sudo dnf install ccache
 sudo dnf install bison
 sudo dnf install flex
-
-# Compile and run SP1 Emulator
-bash run.sh
